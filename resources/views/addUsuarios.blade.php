73b7a5cdc5a432d99dfc1e00fb90df2dff0ca8b3
xabilazkano <xlazcanoit18dw@ikzubirimanteo.com>
2019-11-28 08:09:33 +0100

    Estilos añadidos

--- resources/views/addUsuarios.blade.php
+++ resources/views/addUsuarios.blade.php
@@ -7,12 +7,12 @@
     <meta name="author" content="">
     <title>Ro-Botika</title>
     <!-- Custom fonts for this theme -->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <!-- Theme CSS -->
-    <link href="css/freelancer.min.css" rel="stylesheet">
+    <link href="{{asset('css/freelancer.min.css')}}" rel="stylesheet">
 </head>
 <body id="page-top">
     <!-- Navigation -->
