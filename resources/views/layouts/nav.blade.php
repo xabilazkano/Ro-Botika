8ac5f52921771b5d81a99be1fb69d6b96899c906
xabilazkano <xlazcanoit18dw@ikzubirimanteo.com>
2019-11-29 12:17:18 +0100

    Correccion de navbar

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -7,10 +7,6 @@
                 <li class="nav-item mx-1 mx-lg-1">
                     <a class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger" href="{{route('welcome')}}">{{ __('messages.inicio') }}</a>
                 </li>
-                <li class="nav-item mx-1 mx-lg-1">
-                    <a class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger" href="{{route('usuarios.index')}}">{{ __('messages.usuarios') }}</a>
-                </li>
-
                 <li class="nav-item mx-1 mx-lg-1"><a class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger" href="{{ url('locale/en') }}" ><i class="fa fa-language"></i> EN</a></li>
                 <li class="nav-item mx-1 mx-lg-1"><a class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger" href="{{ url('locale/es') }}" ><i class="fa fa-language"></i> ES</a></li>
                 <li class="nav-item mx-1 mx-lg-1"><a class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger" href="{{ url('locale/eu') }}" ><i class="fa fa-language"></i> EU</a></li>
