226346bb0618c805814061f4387efbed52a889cd
xabilazkano <xlazcanoit18dw@ikzubirimanteo.com>
2019-11-29 09:19:05 +0100

    Borrar vendor

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,7 +24,7 @@
         <p class="masthead-subheading font-weight-light mb-0">
             Los enfermeros y enfermeras realizan un sobreesfuerzo físico al tener que transportar los carros medicinales por todo el hospital. Para contrarrestar esta situación, nuestra idea es ofrecer un sistema de transporte automatizado que se encargará de realizar dicha función sin ninguna otra necesidad del enfermero que la de pulsar en una pantalla táctil.
             <br><br>
-            Queremos que los trabajadores del hospital no tengan que hacer tanto esfuerzo físico en sus labores y así poder estar con más energía a la hora de atender a los enfermos. Creemos que el cansancio es un factor a tener en cuenta cuando hablamos del estado anímico de una persona. Por ello uno de nuestros objetivos es  facilitar todo lo que podamos el trabajo de las enfermeras para que así puedan ser más eficientes.
+            Queremos que los trabajadores del hospital no tengan que hacer tanto esfuerzo físico en sus labores y así poder estar con más energía a la hora de atender a los enfermos. Creemos que el cansancio es un factor a tener en cuenta cuando hablamos del estado anímico de una persona. Por ello uno de nuestros objetivos es facilitar todo lo que podamos el trabajo de las enfermeras para que así puedan ser más eficientes.
         </p>
     </div>
 </section>
