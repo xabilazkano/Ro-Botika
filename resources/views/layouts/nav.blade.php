4a4a4b6a913daf761cb0c3eafe9e79d0be7e5d35
xabilazkano <xlazcanoit18dw@ikzubirimanteo.com>
2019-12-02 08:51:30 +0100

    Login y registro con ventanas modales

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -7,9 +7,6 @@
         <li class="nav-item mx-1 mx-lg-1">
           <a class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger" href="{{route('welcome')}}">{{ __('messages.inicio') }}</a>
         </li>
-        <li class="nav-item mx-1 mx-lg-1">
-          <a class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger" href="{{route('usuarios.index')}}">{{ __('messages.usuarios') }}</a>
-        </li>
 
         <li class="nav-item mx-1 mx-lg-1"><a class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger" href="{{ url('locale/en') }}" ><i class="fa fa-language"></i> EN</a></li>
         <li class="nav-item mx-1 mx-lg-1"><a class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger" href="{{ url('locale/es') }}" ><i class="fa fa-language"></i> ES</a></li>
@@ -18,7 +15,7 @@
 
         @guest
         <li class="nav-item mx-1 mx-lg-1">
-          <a class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger login" href="{{ route('login') }}">{{ __('messages.Iniciar sesión') }}</a>
+          <a style="color: #2c3e50" class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger login bg-light" href="#loginModal" data-toggle="modal">{{ __('messages.Iniciar sesión') }}</a>
         </li>
         @if (Route::has('register'))
         <li class="nav-item mx-1 mx-lg-1">
