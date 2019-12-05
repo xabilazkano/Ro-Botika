861781eed7e41e7dab2d3901e02da8f7ea8bbd8a
AnderGonzalezA <agonzalezam18dw@ikzubirimanteo.com>
2019-12-05 09:04:55 +0100

    ubicacion de las traducciones actualizada en la barra de navegacion

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -7,9 +7,6 @@
                 <li class="nav-item mx-1 mx-lg-1">
                     <a class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger" href="{{route('welcome')}}">{{ __('messages.inicio') }}</a>
                 </li>
-                <li class="nav-item mx-1 mx-lg-1"><a class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger" href="{{ url('locale/en') }}" ><i class="fa fa-language"></i> EN</a></li>
-                <li class="nav-item mx-1 mx-lg-1"><a class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger" href="{{ url('locale/es') }}" ><i class="fa fa-language"></i> ES</a></li>
-                <li class="nav-item mx-1 mx-lg-1"><a class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger" href="{{ url('locale/eu') }}" ><i class="fa fa-language"></i> EU</a></li>
                 @guest
                 <li class="nav-item mx-1 mx-lg-1">
                     <a style="color: #1abc9c" class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger login" href="#loginModal" data-toggle="modal">{{ __('messages.Iniciar sesión') }}</a>
@@ -47,6 +44,16 @@
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" >
                             @csrf
                         </form>
+                    </div>
+                </li>
+                <li class="nav-item dropdown mx-1 mx-lg-1">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle py-3 px-3 px-lg-3 rounded js-scroll-trigger" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                         <span class="caret"><i class="fa fa-language"></i></span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ url('locale/en') }}">EN</a>
+                        <a class="dropdown-item" href="{{ url('locale/es') }}">ES</a>
+                        <a class="dropdown-item" href="{{ url('locale/eu') }}">EU</a>
                     </div>
                 </li>
                 @endif
