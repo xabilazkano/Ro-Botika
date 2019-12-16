05f51cb7cf9523e7fdfbd475d43850fe0d39f763
Ekain Susperregi <suspekain@gmail.com>
2019-12-16 12:53:56 +0100

    migr.patients

---
+++ database/migrations/2019_12_16_114424_create_patient_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('ss_number');
+            $table->string('name');
+            $table->string('lastname');
+            $table->string('disease');
+            $table->string('bed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patient');
+    }
+}
