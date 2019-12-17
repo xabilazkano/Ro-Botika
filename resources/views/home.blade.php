4002067c4368f985049bc7cb67d6dac4e95c6133
Ekain Susperregi <suspekain@gmail.com>
2019-12-17 11:31:38 +0100

    home standar aldaketa

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,24 +2,12 @@
 
 @section('content')
 <section class="masthead text-center">
-   <div class="container d-flex align-items-center flex-column">
-    <div class="col-md-8">
-        <div class="card">
-            <div class="card-header">Dashboard</div>
-
-            <div class="card-body">
-                @if (session('status'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('status') }}
-                </div>
-                @endif
-                @if(Auth::user()->hasRole('admin'))
-                    {{__('messages.mensajeInicioSesionAdmin')}}
-                @else
-                    {{__('messages.mensajeInicioSesionStandar')}}
-                @endif
-            </div>
-        </div>
+   <div class="container d-flex align-items-center">
+    <div class="col-md-6">
+      <button type="button" class="btn btn-secondary">Light</button>
+    </div>
+    <div class="col-md-6">
+      <button type="button" class="btn btn-secondary">Light</button>
     </div>
 </div>
 </section>
