fbd628b10c7459f2972a24629deb7a1c4aee005d
Ekain Susperregi <suspekain@gmail.com>
2019-12-17 11:37:43 +0100

    ikonoak jarrita

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,10 @@
 <section class="masthead text-center">
    <div class="container d-flex align-items-center">
     <div class="col-md-6">
-      <button type="button" class="btn btn-secondary">Light</button>
+      <button type="button" class="btn btn-secondary d-flex flex-row">
+        <i id="patienticon" class="fa fa-user" aria-hidden="true"></i>
+        PACIENTES
+      </button>
     </div>
     <div class="col-md-6">
       <button type="button" class="btn btn-secondary">Light</button>
