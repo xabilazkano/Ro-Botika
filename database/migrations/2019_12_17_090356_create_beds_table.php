ff26ac95832d40cdcdf1acc5db941b8c815ccc4f
AnderGonzalezA <agonzalezam18dw@ikzubirimanteo.com>
2019-12-17 10:42:19 +0100

    base de datos terminada

--- database/migrations/2019_12_17_090356_create_beds_table.php
+++ database/migrations/2019_12_17_090356_create_beds_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePatientTable extends Migration
+class CreateBedsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreatePatientTable extends Migration
      */
     public function up()
     {
-        Schema::create('patients', function (Blueprint $table) {
+        Schema::create('beds', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('ss_number');
-            $table->string('name');
-            $table->string('lastname');
-            $table->string('disease');
+            $table->integer('floor');
+            $table->integer('room');
+            $table->char('bed',1);
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ class CreatePatientTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('patient');
+        Schema::dropIfExists('beds');
     }
 }
---
+++ database/migrations/2019_12_17_090552_create_beds_patients_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBedsPatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beds_patients', function (Blueprint $table) {
+            $table->unsignedBigInteger('bed_id');
+            $table->foreign('bed_id')->references('id')->on('beds');
+            $table->unsignedBigInteger('patient_id');
+            $table->foreign('patient_id')->references('id')->on('patients');
+            $table->date('up_date');
+            $table->date('down_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beds_patients');
+    }
+}
