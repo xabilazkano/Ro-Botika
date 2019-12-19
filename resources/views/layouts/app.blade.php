a1fe833c0cd6d0cfef5a2d02cfc93504d8d08d13
xabilazkano <xlazcanoit18dw@ikzubirimanteo.com>
2019-12-19 11:01:24 +0100

    nuevo navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,25 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    @include('layouts.head')
+@include('layouts.head')
 <body>
   <div class="row">
-    <div class="col-3">
+    <div class="col-12 content">
       @include('layouts.nav')
-    </div>
-    <div class="col-9 content">
       @include('layouts.modals')
       <main class="py-4">
         @yield('content')
       </main>
     </div>
   </div>
+  <script>
+    $(document).ready(function(){
+      $('.dropdown-submenu a.test').on("click", function(e){
+        $(this).next('ul').toggle();
+        e.stopPropagation();
+        e.preventDefault();
+      });
+    });
+  </script>
+
 </body>
 </html>
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,68 +1,73 @@
-<!-- Navigation -->
+<nav class="navbar text-uppercase" id="mainNav">
 
-<nav class="navbar navbar-expand-lg bg-secondary text-uppercase vertical" id="mainNav">
-    <div class="container flex-column justify-content-between">
-        <div id="landing">
-           <a class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger text-light" href="">Landing Page</a>
-       </div>
-       <div id="nav">
-        <a class="navbar-brand js-scroll-trigger" href="{{route('welcome')}}">Ro-Botika</a>
-        <div id="navbarResponsive">
-            <ul class="navbar-nav ml-auto flex-column">
-                <li class="nav-item mx-1 mx-lg-1">
-                    <a class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger" href="{{route('welcome')}}">{{ __('messages.inicio') }}</a>
-                </li>
-                @guest
-                <li class="nav-item mx-1 mx-lg-1">
-                    <a style="color: #1abc9c" class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger login" href="#loginModal" data-toggle="modal">{{ __('messages.Iniciar sesión') }}</a>
-                </li>
-                @else
-                @if (Auth::user()->hasVerifiedEmail())
-                <li class="nav-item dropdown mx-1 mx-lg-1">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle py-3 px-3 px-lg-3 rounded js-scroll-trigger" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }} <span class="caret"></span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('homeAdmin')}}">
-                          @if(Auth::user()->hasRole('admin'))
-                          {{__('messages.Panel de administrador')}}
-                          @else
-                          {{__('messages.Panel estándar')}}
-                          @endif
-                      </a>
-                      <a class="dropdown-item" href="{{ route('usuarios.edit',Auth::user()->id) }}">
-                        {{ __('messages.Modificar usuario') }}
-                    </a>
-                    <a class="dropdown-item" href="{{ route('usuarios.delete',Auth::user()->id) }}">
-                        {{ __('messages.Eliminar usuario') }}
-                    </a>
-                    <a class="dropdown-item" href="{{ route('logout') }}"
-                    onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();">
-                    {{ __('messages.Cerrar sesión') }}
-                </a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" >
-                    @csrf
-                </form>
-            </div>
-        </li>
-        @endif
-        @endguest
-        <li class="nav-item dropdown mx-1 mx-lg-1">
-            <a id="navbarDropdown" class="nav-link dropdown-toggle py-3 px-3 px-lg-3 rounded js-scroll-trigger" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-             <span class="caret"><i class="fa fa-language"></i></span>
-         </a>
-         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="{{ url('locale/en') }}">EN</a>
-            <a class="dropdown-item" href="{{ url('locale/es') }}">ES</a>
-            <a class="dropdown-item" href="{{ url('locale/eu') }}">EU</a>
-        </div>
-    </li>
-</ul>
-</div>
-</div>
-<div id="help" class="text-center">
-    <i class="fa fa-question-circle fa-3x text-light"></i>
-</div>
+
+
+	<a class="text-dark fa-3x" data-toggle="dropdown" href="#">
+		<i class="fa fa-bars"></i>
+	</a>
+	<div class="col-md-2">
+		<ul class="dropdown-menu">
+			<li><a tabindex="-1" class="navbar-brand js-scroll-trigger text-dark" href="{{route('welcome')}}">Ro-Botika</a></li>
+			<li><a tabindex="-1" class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger text-dark" href="">Landing Page</a></li>
+			<li><a tabindex="-1" class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger text-dark" href="{{route('welcome')}}">{{ __('messages.inicio') }}</a></li>
+			@guest
+			<li><a tabindex="-1" class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger text-dark" href="#loginModal" data-toggle="modal">{{ __('messages.Iniciar sesión') }}</a></li>
+			@else
+			@if (Auth::user()->hasVerifiedEmail())
+			<li class="dropdown-submenu">
+				<a class="test py-3 px-3 px-lg-3 rounded text-dark"  tabindex="-1" href="#">{{ Auth::user()->name }}
+					<ul class="dropdown-menu">
+						<li>
+							<a tabindex="-1" class="dropdown-item" href="{{ route('homeAdmin')}}">
+								@if(Auth::user()->hasRole('admin'))
+								{{__('messages.Panel de administrador')}}
+								@else
+								{{__('messages.Panel estándar')}}
+								@endif
+							</a>
+						</li>
+						<li>
+							<a tabindex="-1" class="dropdown-item" href="{{ route('usuarios.edit',Auth::user()->id) }}">
+								{{ __('messages.Modificar usuario') }}
+							</a>
+						</li>
+						<li>
+							<a tabindex="-1" class="dropdown-item" href="{{ route('usuarios.delete',Auth::user()->id) }}">
+								{{ __('messages.Eliminar usuario') }}
+							</a>
+						</li>
+						<li>
+							<a tabindex="-1" class="dropdown-item" href="{{ route('logout') }}"
+							onclick="event.preventDefault();
+							document.getElementById('logout-form').submit();">
+							{{ __('messages.Cerrar sesión') }}
+						</a>
+						<form id="logout-form" action="{{ route('logout') }}" method="POST" >
+							@csrf
+						</form>
+					</li>
+
+				</ul>
+			</a>
+		</li>
+		@endif
+		@endguest
+		<li class="dropdown-submenu">
+			<a class="test py-3 px-3 px-lg-3 rounded text-dark" tabindex="-1" href="#"> <span class="caret"><i class="fa fa-language"></i></span>
+				<ul class="dropdown-menu">
+					<li>
+						<a tabindex="-1" class="dropdown-item" href="{{ url('locale/en') }}">EN</a>
+					</li>
+					<li>
+						<a tabindex="-1" class="dropdown-item" href="{{ url('locale/es') }}">ES</a>
+					</li>
+					<li>
+						<a tabindex="-1" class="dropdown-item" href="{{ url('locale/eu') }}">EUS</a>
+					</li>
+				</ul>
+			</a>
+		</li>
+		<li class="text-center"><i class="fa fa-question-circle fa-3x text-dark"></i></li>
+	</ul>
 </div>
 </nav>
