979e247e843cd950c74e7dce9ae7d1d77cec20d3
Ekain Susperregi <suspekain@gmail.com>
2019-12-19 11:20:19 +0100

    button itxita

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,8 +11,9 @@
     </div>
     <div class="col-md-6">
       <button type="button" class="btn btn-secondary d-flex flex-row justify-content-around align-items-center">
-        <i class="patienticon fas fa-procedures"></i>
+        <i class="patienticon fa fa-bed"></i>
         <h2>CAMAS</h2>
+      </button>
     </div>
 </div>
 </section>
