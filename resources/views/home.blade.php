e1fa0ef21f5efb9d84868fca69601d88e84963a2
AnderGonzalezA <agonzalezam18dw@ikzubirimanteo.com>
2019-12-20 11:00:26 +0100

    enlaces a rutas añadidos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <section class="masthead text-center">
   <div class="container d-flex align-content-around flex-wrap">
     <div class="col-md-6">
-      <a href="#">
+      <a href="{{route('patients.index')}}">
         <button type="button" class="botonhome btn btn-secondary d-flex flex-row justify-content-around align-items-center">
           <i class="patienticon fa fa-user"></i>
           <h2>PACIENTES</h2>
@@ -12,7 +12,7 @@
       </a>
     </div>
     <div class="col-md-6">
-      <a href="#">
+      <a href="{{route('beds.index')}}">
         <button type="button" class="botonhome btn btn-secondary d-flex flex-row justify-content-around align-items-center">
           <i class="patienticon fa fa-home"></i>
           <h2>CAMAS</h2>
@@ -21,7 +21,7 @@
     </div>
     <br>
     <div class="col-md-6">
-      <a href="#">
+      <a href="{{route('assistances.index')}}">
         <button type="button" class="botonhome btn btn-secondary d-flex flex-row justify-content-around align-items-center">
           <i class="patienticon fa fa-stethoscope"></i>
           <h2>ASISTENCIAS</h2>
