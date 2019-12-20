eb80d67d45c215dffdb2aa5afb8759448c99f622
Ekain Susperregi <suspekain@gmail.com>
2019-12-20 09:03:18 +0100

    4botoiak jarri

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,19 +2,32 @@
 
 @section('content')
 <section class="masthead text-center">
-   <div class="container d-flex align-items-center">
-    <div class="col-md-6">
-      <button type="button" class="btn btn-secondary d-flex flex-row justify-content-around align-items-center">
-        <i class="patienticon fa fa-user"></i>
-        <h2>PACIENTES</h2>
-      </button>
-    </div>
-    <div class="col-md-6">
-      <button type="button" class="btn btn-secondary d-flex flex-row justify-content-around align-items-center">
-        <i class="patienticon fa fa-bed"></i>
-        <h2>CAMAS</h2>
-      </button>
-    </div>
+   <div class="container d-flex align-content-around flex-wrap">
+       <div class="col-md-6">
+         <button type="button" class="btn btn-secondary d-flex flex-row justify-content-around align-items-center">
+           <i class="patienticon fa fa-user"></i>
+           <h2>PACIENTES</h2>
+         </button>
+       </div>
+       <div class="col-md-6">
+         <button type="button" class="btn btn-secondary d-flex flex-row justify-content-around align-items-center">
+           <i class="patienticon fa fa-user"></i>
+           <h2>PACIENTES</h2>
+         </button>
+       </div>
+       <br>
+       <div class="col-md-6">
+         <button type="button" class="btn btn-secondary d-flex flex-row justify-content-around align-items-center">
+           <i class="patienticon fa fa-user"></i>
+           <h2>PACIENTES</h2>
+         </button>
+       </div>
+       <div class="col-md-6">
+         <button type="button" class="btn btn-secondary d-flex flex-row justify-content-around align-items-center">
+           <i class="patienticon fa fa-user"></i>
+           <h2>PACIENTES</h2>
+         </button>
+       </div>
 </div>
 </section>
 @endsection
