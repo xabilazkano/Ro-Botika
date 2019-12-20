a174bada576f0ad268a42abaab700a3fdb6f86b2
Ekain Susperregi <suspekain@gmail.com>
2019-12-20 10:53:04 +0100

    icons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,28 +4,28 @@
 <section class="masthead text-center">
    <div class="container d-flex align-content-around flex-wrap">
        <div class="col-md-6">
-         <button type="button" class="btn btn-secondary d-flex flex-row justify-content-around align-items-center">
+         <button type="button" class="botonhome btn btn-secondary d-flex flex-row justify-content-around align-items-center">
            <i class="patienticon fa fa-user"></i>
            <h2>PACIENTES</h2>
          </button>
        </div>
        <div class="col-md-6">
-         <button type="button" class="btn btn-secondary d-flex flex-row justify-content-around align-items-center">
-           <i class="patienticon fa fa-user"></i>
-           <h2>PACIENTES</h2>
+         <button type="button" class="botonhome btn btn-secondary d-flex flex-row justify-content-around align-items-center">
+           <i class="patienticon fa fa-home"></i>
+           <h2>CAMAS</h2>
          </button>
        </div>
        <br>
        <div class="col-md-6">
-         <button type="button" class="btn btn-secondary d-flex flex-row justify-content-around align-items-center">
-           <i class="patienticon fa fa-user"></i>
-           <h2>PACIENTES</h2>
+         <button type="button" class="botonhome btn btn-secondary d-flex flex-row justify-content-around align-items-center">
+           <i class="patienticon fa fa-stethoscope"></i>
+           <h2>ASISTENCIAS</h2>
          </button>
        </div>
        <div class="col-md-6">
-         <button type="button" class="btn btn-secondary d-flex flex-row justify-content-around align-items-center">
-           <i class="patienticon fa fa-user"></i>
-           <h2>PACIENTES</h2>
+         <button type="button" class="botonhome btn btn-secondary d-flex flex-row justify-content-around align-items-center">
+           <i class="patienticon fa fa-list-alt"></i>
+           <h2>STOCK</h2>
          </button>
        </div>
 </div>
