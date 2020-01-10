f7ac5752db65ac1d3af50f97742c2e73fb94a064
Ekain Susperregi <suspekain@gmail.com>
2020-01-10 10:21:47 +0100

    pwd factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,7 +22,7 @@ $factory->define(User::class, function (Faker $faker) {
         'lastname'=>$faker->lastname,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => Hash::make('secret'), // password
         'remember_token' => Str::random(10),
         'phone_number'=>$faker->phoneNumber,
         'type_of_user'=>'nurse'
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,8 +21,6 @@
         </button>
       </a>
     </div>
-
-    @if (Auth::user()->hasRole("nurse"))
     <div class="col-md-6">
       <a href="{{route('rooms.index')}}">
         <button type="button" class="botonhome btn btn-secondary d-flex flex-row justify-content-around align-items-center">
@@ -39,34 +37,6 @@
         </button>
       </a>
     </div>
-    @endif
-
-    @if (Auth::user()->hasRole("admin"))
-    <div class="col-md-4">
-      <a href="{{route('rooms.index')}}">
-        <button type="button" class="botonhome btn btn-secondary d-flex flex-row justify-content-around align-items-center">
-          <i class="patienticon fa fa-home"></i>
-          <h2>HABITACIONES</h2>
-        </button>
-      </a>
-    </div>
-    <div class="col-md-4">
-      <a href="{{route('medicines.index')}}">
-        <button type="button" class="botonhome btn btn-secondary d-flex flex-row justify-content-around align-items-center">
-          <i class="patienticon fa fa-list-alt"></i>
-          <h2>STOCK</h2>
-        </button>
-      </a>
-    </div>
-    <div class="col-md-4">
-      <a href="{{route('adminNurses.index')}}">
-        <button type="button" class="botonhome btn btn-secondary d-flex flex-row justify-content-around align-items-center">
-          <h2>ENFERMER@S</h2>
-        </button>
-      </a>
-    </div>
-    @endif
-
   </div>
 </section>
 @endsection
