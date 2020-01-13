25f036eefaf5155d51ca9cdd260525fc4a0989c4
Ekain Susperregi <suspekain@gmail.com>
2020-01-13 10:20:34 +0100

    columna cama en tabla + seeder room-patient

--- database/factories/RoomFactory.php
+++ database/factories/RoomFactory.php
@@ -9,6 +9,6 @@ $factory->define(Room::class, function (Faker $faker) {
     return [
       'floor' => $faker->numberBetween($min = 0, $max = 4),
       'room_number' => $faker->numberBetween($min = 1, $max = 30),
-      'beds' => $faker->numberBetween($min = 1, $max = 4),
+      'beds' => 2,
     ];
 });
--- database/seeds/PatientsRoomsTableSeeder.php
+++ database/seeds/PatientsRoomsTableSeeder.php
@@ -4,26 +4,33 @@ use Illuminate\Database\Seeder;
 
 class PatientsRoomsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $fecha = date('Y/m/d');
-    	for ($i=0; $i < 20; $i++) {
-    		$up_date = strtotime ( "+".rand(1,31)." day" , strtotime ( $fecha ) ) ;
-    		$up_date = date ( 'Y/m/d' , $up_date);
+  /**
+  * Run the database seeds.
+  *
+  * @return void
+  */
+  public function run()
+  {
+    $fecha = date('Y/m/d');
+    for ($i=1; $i < 21; $i++) {
+      $up_date = strtotime ( "+".rand(1,31)." day" , strtotime ( $fecha ) ) ;
+      $up_date = date ( 'Y/m/d' , $up_date);
 
-    		$down_date = strtotime ( "+".rand(30,500)." day" , strtotime ( $fecha ) ) ;
-    		$down_date = date ( 'Y/m/d' , $down_date );
-    		DB::table('patient_room')->insert([
-    			'room_id' => rand(1,20),
-    			'patient_id' => rand(1,20),
-    			'up_date' => $up_date,
-    			'down_date' => $down_date
-    		]);
-    	}
+      $down_date = strtotime ( "+".rand(30,500)." day" , strtotime ( $fecha ) ) ;
+      $down_date = date ( 'Y/m/d' , $down_date );
+
+      if (($i%2) === 0) {
+        $bed = 'B';
+      } else {
+        $bed = 'A';
+      }
+      DB::table('patient_room')->insert([
+        'room_id' => rand(1,20),
+        'bed' => $bed,
+        'patient_id' => rand(1,20),
+        'up_date' => $up_date,
+        'down_date' => $down_date
+      ]);
     }
+  }
 }
