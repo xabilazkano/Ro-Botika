2b9bdb5fbbb8a7f291743c87b17f1b734b483b55
AnderGonzalezA <agonzalezam18dw@ikzubirimanteo.com>
2020-01-27 13:32:24 +0100

    solo pacientes actuales y habitaciones ocupadas (nurse)

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -7,22 +7,23 @@
 			<th>{{__('messages.planta')}}</th>
 			<th>{{__('messages.numerohabitacion')}}</th>
 			<th>{{__('messages.camas')}}</th>
+			<th>{{__('messages.Pacientes')}}</th>
 			<th></th>
 		</tr>
 	</thead>
 	@foreach ($rooms as $room)
 				<tr>
 					<td>{{$room->floor}}</td>
-					<td>{{$room->room_number}}</td>
+					<td>{{$room->id}}</td>
 					<td>{{$room->beds}}</td>
 					<td>
 					@foreach ($room->patients as $patient)
-						@if ($patient->pivot->up_date <= date('Y-m-d') && $patient->pivot->down_date >= date('Y-m-d'))
+						@if ($patient->pivot->up_date <= date('Y-m-d') && ($patient->pivot->down_date >= date('Y-m-d') || $patient->pivot->down_date === null))
 							<a href="{{route('patients.show',$patient->id)}}">{{$patient->name}} {{$patient->lastname}}</a><br>
 						@endif
 					@endforeach
 				</td>
-				<td><a href="{{route('rooms.show',$room->room_number)}}"><i class="blackIcon fa fa-eye"></i></a></td>
+				<td><a href="{{route('rooms.show',$room->id)}}"><i class="blackIcon fa fa-eye"></i></a></td>
 				</tr>
 	@endforeach
 </table>
