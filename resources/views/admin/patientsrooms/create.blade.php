c54ccfff2986dcb27228d87752204e588e4d5a88
AnderGonzalezA <agonzalezam18dw@ikzubirimanteo.com>
2020-01-28 10:22:10 +0100

    al querer añadir un patient room desplegar solo las habitaciones con una cama libre como minimo

--- resources/views/admin/patientsrooms/create.blade.php
+++ resources/views/admin/patientsrooms/create.blade.php
@@ -39,7 +39,7 @@
               <?php
                 $camasOcupadas = 0;
                 foreach ($patientsRooms as $patientRoom){
-                  if ($patientRoom->room_id == $room->id  && $patientRoom->up_date<=date('Y-m-d') && $patientRoom->down_date>=date('Y-m-d')){
+                  if ($patientRoom->room_id == $room->id  && $patientRoom->up_date<=date('Y-m-d') && ($patientRoom->down_date >= date('Y-m-d') || !isset($patientRoom->down_date))){
                     $camasOcupadas++;
                   }
                 }
