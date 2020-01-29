bfde4540af51185e7ff9ecb28855a10ed8f1e68d
Ekain Susperregi <suspekain@gmail.com>
2020-01-29 13:15:15 +0100

    separado, y boton de asistencia quitada

--- resources/views/assistances/index.blade.php
+++ resources/views/assistances/index.blade.php
@@ -14,6 +14,7 @@
 		</tr>
 	</thead>
 	@foreach ($assistances as $assist)
+		@if ($assist->confirmed != 1)
 		@if ($assist->estimated_date === date('Y-m-d'))
 			<tr>
 				<td><a href="{{route('patients.show',$assist->patient->id)}}">{{$assist->patient->name}} {{$assist->patient->lastname}}</a></td>
@@ -40,6 +41,47 @@
 					</a>
 				</td>
 			</tr>
+		@endif
+		@endif
+		@endforeach
+</table>
+<hr><br>
+<h3>{{__('messages.AsistenciasConfirmadas')}}</h3>
+<table class="table">
+	<thead class="thead">
+		<tr>
+			<th>{{__('messages.Paciente')}}</th>
+			<th>{{__('messages.Enfermera')}}</th>
+			<th>{{__('messages.Fecha')}}</th>
+			<th>{{__('messages.Medicinas')}}</th>
+			<th>{{__('messages.Confirmado')}}</th>
+			<th></th>
+		</tr>
+	</thead>
+	@foreach ($assistances as $assist)
+		@if ($assist->confirmed == 1)
+		@if ($assist->estimated_date === date('Y-m-d'))
+			<tr>
+				<td><a href="{{route('patients.show',$assist->patient->id)}}">{{$assist->patient->name}} {{$assist->patient->lastname}}</a></td>
+				<td>{{$assist->user->name}}</td>
+				<td>{{$assist->estimated_date}}</td>
+				<td>
+					@foreach ($assist->medicines as $medicine)
+					<a href="{{route('medicines.show',$medicine->id)}}">{{$medicine->name}}</a><br>
+					@endforeach
+				</td>
+				<td>
+					@if (is_null($assist->confirmed))
+					<i class=" blackIcon fa fa-question"></i>
+					@else
+					<i class=" confirm fa fa-check"></i>
+					@endif
+				</td>
+				<td>
+					<a href="{{route('assistances.show',$assist->id)}}"><i class="blackIcon fa fa-eye"></i></a>
+				</td>
+			</tr>
+		@endif
 		@endif
 	@endforeach
 </table>
