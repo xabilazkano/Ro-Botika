d5c53b019ad4a042166a49ca648e5d1bec9b2e54
AnderGonzalezA <agonzalezam18dw@ikzubirimanteo.com>
2020-01-29 09:15:56 +0100

    documentacion corregida api server

--- app/Http/Controllers/AssistanceController.php
+++ app/Http/Controllers/AssistanceController.php
@@ -12,7 +12,7 @@ use App\PatientRoom;
 /**
 * @OA\Info(title="API Carro Automatizado", version="1.0")
 *
-* @OA\Server(url="http://localhost:8000")
+* @OA\Server(url="http://robotika.ddns.net/")
 */
 
 class AssistanceController extends Controller
