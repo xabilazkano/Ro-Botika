dd1a0d1a82ea59f7c0388d0f73ee83cb6d6345b7
Ekain Susperregi <suspekain@gmail.com>
2020-02-03 11:04:58 +0100

    zuzenketa batzuk

--- resources/views/admin/layoutsAdmin/app.blade.php
+++ resources/views/admin/layoutsAdmin/app.blade.php
@@ -4,12 +4,13 @@
 <body>
   @include('admin.layoutsAdmin.nav')
   <div class="container-fluid pb-5">
-    <div class="row">
+    <div class="row d-flex">
+      <nav class="col-md-2 col-sm-12 d-none d-md-block bg-light sidebar">
         @include('admin.layoutsAdmin.sideNav')
-        @yield('content')
-        @include('admin.layoutsAdmin.footer')
-      </div>
-
+      </nav>
+      @yield('content')
+      @include('admin.layoutsAdmin.footer')
+    </div>
   </div>
   @include('admin.layoutsAdmin.footer')
 </body>
