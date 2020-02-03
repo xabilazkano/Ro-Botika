baf9a18a25038af2863cf85fe6515838a25e38d5
xabilazkano <xlazcanoit18dw@ikzubirimanteo.com>
2020-02-03 13:52:33 +0100

    Redes sociales

--- resources/views/admin/layoutsAdmin/footer.blade.php
+++ resources/views/admin/layoutsAdmin/footer.blade.php
@@ -2,13 +2,13 @@
 <footer class="py-2 bg-dark footer text-center col-12">
   <div class="container">
     <a class="btn btn-outline-light btn-social mx-1" href="http://www.forocoches.com">
-      <i class="fas fa-facebook"></i>
+      <i class="fab fa-facebook"></i>
     </a>
     <a class="btn btn-outline-light btn-social mx-1" href="">
-      <i class="fas fa-twitter"></i>
+      <i class="fab fa-twitter"></i>
     </a>
     <a class="btn btn-outline-light btn-social mx-1" href="">
-      <i class="fas fa-linkedin"></i>
+      <i class="fab fa-linkedin"></i>
     </a>
   </div>
   <!-- /.container -->
