fbef28ee7472bc9cffe0039ae956c3e2f9fbbc31
Ekain Susperregi <suspekain@gmail.com>
2020-02-05 10:43:17 +0100

    auto indent

--- resources/views/landingpage/layouts/nav.blade.php
+++ resources/views/landingpage/layouts/nav.blade.php
@@ -1,22 +1,22 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-    <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="{{route('landingpage')}}">Ro-Botika</a>
-        <div id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item mx-1 mx-lg-1">
-                    <a class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger" href="{{route('homeAdmin')}}">{{__('messages.Volver a la aplicación')}}</a>
-                </li>
-                <div class="btn-group btn-group-inline">
-          				<button class="btn" data-toggle="dropdown">
-          					<span class="caret"><i class="iconoIdiomas fa fa-language fa-2x"></i></span>
-          				</button>
-          				<ul class="dropdown-menu">
-          					<li><a tabindex="-1" class="dropdown-item" href="{{ url('locale/en') }}">EN</a></li>
-          					<li><a tabindex="-1" class="dropdown-item" href="{{ url('locale/es') }}">ES</a></li>
-          					<li><a tabindex="-1" class="dropdown-item" href="{{ url('locale/eu') }}">EUS</a></li>
-          				</ul>
-          			</div>
-            </div>
+  <div class="container">
+    <a class="navbar-brand js-scroll-trigger" href="{{route('landingpage')}}">Ro-Botika</a>
+    <div id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item mx-1 mx-lg-1">
+          <a class="nav-link py-3 px-3 px-lg-3 rounded js-scroll-trigger" href="{{route('homeAdmin')}}">{{__('messages.Volver a la aplicación')}}</a>
+        </li>
+        <div class="btn-group btn-group-inline">
+          <button class="btn" data-toggle="dropdown">
+            <span class="caret"><i class="iconoIdiomas fa fa-language fa-2x"></i></span>
+          </button>
+          <ul class="dropdown-menu">
+            <li><a tabindex="-1" class="dropdown-item" href="{{ url('locale/en') }}">EN</a></li>
+            <li><a tabindex="-1" class="dropdown-item" href="{{ url('locale/es') }}">ES</a></li>
+            <li><a tabindex="-1" class="dropdown-item" href="{{ url('locale/eu') }}">EUS</a></li>
+          </ul>
         </div>
-    </nav>
+      </div>
+    </div>
+  </nav>
