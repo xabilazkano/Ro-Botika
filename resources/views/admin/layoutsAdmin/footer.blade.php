317c74ec878a702480b2750b36e4ee5fb29bec8f
xabilazkano <xlazcanoit18dw@ikzubirimanteo.com>
2020-02-05 13:25:25 +0100

    Corregir footer admin

--- resources/views/admin/layoutsAdmin/footer.blade.php
+++ resources/views/admin/layoutsAdmin/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- Footer -->
 <footer class="py-2 bg-dark footer text-center col-12">
   <div class="container">
-    <a class="btn btn-outline-light btn-social mx-1" href="http://www.forocoches.com">
+    <a class="btn btn-outline-light btn-social mx-1" href="">
       <i class="fab fa-facebook"></i>
     </a>
     <a class="btn btn-outline-light btn-social mx-1" href="">
