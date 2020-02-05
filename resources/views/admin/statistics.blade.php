b7f0283dcebf809d8a9fa0a8a0c91248162dcc5e
Xabi <xlazcanoit18dw@ikzubirimanteo.com>
2020-02-05 09:30:46 +0100

    Cambiar url de request a api

--- resources/views/admin/statistics.blade.php
+++ resources/views/admin/statistics.blade.php
@@ -68,7 +68,7 @@ var besteak = "";
   $(document).ready(function(){
   $("#calcular").click(function(){
     var fecha = $("#fecha").val();
-    var url = 'http://robotika.ddns.net/api/grafica/'+fecha;
+    var url = 'https://robotika.ddns.net/api/grafica/'+fecha;
     $.get(url,function(data,status){
       if (status === "success"){
 
